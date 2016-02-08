703eadb0dfc4818402fd951ebb5bcddb66435047
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-02-08 15:11:53 +0100

    Aim w. mouse cursor.

---
+++ Ghetto-Blaster/Assets/Game/Scripts/Aim.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+    private Transform aim;
+
+    private Vector3 pos;
+    public float zDistance = -10f;
+
+	// Use this for initialization
+	void Start () {
+        aim = GetComponent<Aim>().transform;
+      //  Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, zDistance);
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        aim.position = pos;
+	}
+}
---
+++ Ghetto-Blaster/Assets/Game/Scripts/WeaponManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponManager : MonoBehaviour {
+
+    private bool canShoot = true;
+    public float shootCooldown = 0.1f;
+
+    public int ammo = 100;
+    public int clipSize = 10;
+    private int currentClip;
+
+	// Use this for initialization
+	void Start () {
+        currentClip = clipSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+
+        // right click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Reload();
+        }
+
+	}
+
+    public void Fire()
+    {
+        // TODO
+        Debug.Log("Break sum foo!");
+    }
+
+    public void Reload()
+    {
+        // TODO
+        Debug.Log("Reloading!");
+    }
+}
